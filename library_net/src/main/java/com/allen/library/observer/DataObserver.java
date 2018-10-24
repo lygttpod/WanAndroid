2f313916b2d6f6dd6b4be68d563b3a646b6460db
xiaoyao <xiaoyao@51vest.com>
2018-10-24 10:02:44 +0800

    修复8.0以上静态广播无法收到消息的bug

--- library_net/src/main/java/com/allen/library/observer/DataObserver.java
+++ library_net/src/main/java/com/allen/library/observer/DataObserver.java
@@ -1,5 +1,6 @@
 package com.allen.library.observer;
 
+import android.content.ComponentName;
 import android.content.Intent;
 import android.text.TextUtils;
 
@@ -65,6 +66,9 @@ public abstract class DataObserver<T> extends BaseDataObserver<T> {
             doOnError(data.getErrorMsg());
             if (data.getErrorMsg().contains("登录")) {
                 Intent intent = new Intent("com.allen.wan_android.need_login_action");
+                //android8.0之后对静态广播做了限制（为了省电），需要加上setComponent这行就没问题了
+                //ComponentName的两个参数，第一个是应用包名(网上很多说是自定义广播的包名，其实应该是应用的包名，查看源码可以的到验证)，第二个是自定义广播的全路径
+                intent.setComponent(new ComponentName("com.allen.wanandroid", "com.allen.wanandroid.broadcast.LoginBroadcastReceiver"));
                 RxHttpUtils.getContext().sendBroadcast(intent);
             }
         }
